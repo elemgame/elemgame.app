0cb120d941effc688ae9fd30a8a6e70e852cc575
Ilyar <ilyar.software@gmail.com>
2024-04-23 18:43:16 +0200

    feat: update roadmap

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,8 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.STATIC ? 'docs' : '',
-  output:  process.env.STATIC ? 'export' : '',
+  distDir: process.env.STATIC ? 'docs' : 'dist',
+  output:  process.env.STATIC ? 'export' : 'output',
   images: {
     unoptimized: true,
   },
