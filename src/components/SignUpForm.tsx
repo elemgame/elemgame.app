c0da3b0e6e15944d87d02608492a0428dcde3478
Ilyar <ilyar.software@gmail.com>
2024-05-07 02:01:12 +0200

    feat: change link into medium

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -3,13 +3,12 @@ import { useId } from 'react'
 import { Button } from '@/components/Button'
 
 export function SignUpForm() {
-  let id = useId()
-
   return (
     <div>
+        <br/>
         <Button className={"w-full flex justify-center items-center text-base"} href="https://oasis.gallery/collection/0:0d16c74a210724fce890494858caa5ef6731cfc34cafc422e97a944cbaf6c042">BUY NOW</Button>
         <br/>
-        <Button className={"w-full flex justify-center items-center text-base"} href="https://hackmd.io/4Y40u3D7TDSk2Nn5h4TSHA?view">RULES OF THE GAME</Button>
+        <Button className={"w-full flex justify-center items-center text-base"} href="https://medium.com/@elemgame/rules-of-elemental-game-v1-598b872d3885">RULES OF THE GAME</Button>
     </div>
   )
 }
