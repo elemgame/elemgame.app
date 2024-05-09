98cf7084b5f706f9778e425be6225e61c61810c4
Ilyar <ilyar.software@gmail.com>
2024-05-10 01:42:15 +0200

    build: create CNAME

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 npm run build-static
 mv docs/_next/static docs/static
 rm docs/index.txt
+echo "elemgame.app" | tee docs/CNAME
 
 sed -i 's/\/_next//g' docs/index.html
 sed -i 's/\/_next//g' docs/404.html
